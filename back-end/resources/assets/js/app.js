e38afac70f55043844b393f153e80c481a1c4f7f
Bonnak <chea.bonnak@gmail.com>
2018-08-21 15:34:16 +0000

    Restructure component folder

--- back-end/resources/assets/js/app.js
+++ back-end/resources/assets/js/app.js
@@ -1,15 +1,15 @@
-require('./bootstrap');
-import Vue from 'vue';
-import router from './router';
-import Application from './components/Application.vue';
+// require('./bootstrap');
+// import Vue from 'vue';
+// import router from './router';
+// import Application from './components/Application.vue';
 
-Vue.component('passport-clients', require('./components/passport/Clients.vue'));
-Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue'));
-Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue'));
-Vue.component('spacer', require('./components/Spacer.vue'));
+// Vue.component('passport-clients', require('./components/passport/Clients.vue'));
+// Vue.component('passport-authorized-clients', require('./components/passport/AuthorizedClients.vue'));
+// Vue.component('passport-personal-access-tokens', require('./components/passport/PersonalAccessTokens.vue'));
+// Vue.component('spacer', require('./components/Spacer.vue'));
 
-const app = new Vue({
-    el: '#application',
-    router,
-    render: h => h(Application)
-});
+// const app = new Vue({
+//     el: '#application',
+//     router,
+//     render: h => h(Application)
+// });
