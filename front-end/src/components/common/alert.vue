02cac1d4fe693fb2f2c8d7990180fc63709123b3
Bonnak <nak2bo@yahoo.com>
2018-08-26 15:30:02 +0700

    User login - create Alert and Waiting component

---
+++ front-end/src/components/common/alert.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="border-l-4 p-4" :class="[classList]" role="alert">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Alert",
+  props: {
+    type: {
+      type: String,
+      default: ""
+    }
+  },
+  computed: {
+    classList() {
+      if (this.type.toLowerCase().trim() === "danger") {
+        return {
+          "bg-red-lightest": true,
+          "border-red": true,
+          "text-red-dark": true
+        };
+      } else if (this.type.toLowerCase().trim() === "warning") {
+        return {
+          "bg-orange-lightest": true,
+          "border-orange": true,
+          "text-orange-dark": true
+        };
+      } else if (this.type.toLowerCase().trim() === "info") {
+        return {
+          "bg-blue-lightest": true,
+          "border-blue": true,
+          "text-blue-dark": true
+        };
+      } else if (this.type.toLowerCase().trim() === "success") {
+        return {
+          "bg-green-lightest": true,
+          "border-green": true,
+          "text-green-dark": true
+        };
+      } else {
+        return {
+          "bg-grey-lightest": true,
+          "border-grey": true,
+          "text-grey-dark": true
+        };
+      }
+    }
+  }
+};
+</script>
---
+++ front-end/src/components/common/waiting.vue
@@ -0,0 +1,23 @@
+<style scoped>
+.spin {
+  animation: spin 1s linear infinite;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
+
+<template>
+    <span class="fas fa-spinner spin" aria-hidden="true"></span>
+</template>
+
+<script>
+export default {
+  name: "waiting"
+};
+</script>
