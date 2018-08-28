7e25d79b81dc611002843586489189852a23378b
Bonnak <nak2bo@yahoo.com>
2018-08-28 23:54:15 +0700

    User login - detect if store not yet created in http client

--- front-end/src/http-client.js
+++ front-end/src/http-client.js
@@ -10,11 +10,11 @@ const getClient = (baseUrl = process.env.VUE_APP_API_URL) => {
     }
   }
 
-  // if (store.getters['auth/isAuthenticated']) {
-  //   options.headers = {
-  //     Authorization: `Bearer ${store.getters['auth/accessToken']}`
-  //   }
-  // }
+  if (store && store.getters['auth/isAuthenticated']) {
+    options.headers = {
+      Authorization: `Bearer ${store.getters['auth/accessToken']}`
+    }
+  }
 
   const client = axios.create(options)
 
