3e0fe17011625f2d6211ddba1e781871b5f9082b
Bonnak <nak2bo@yahoo.com>
2018-09-02 14:54:15 +0700

    Manage user - button sizing

--- frontend/src/components/common/_base-button.vue
+++ frontend/src/components/common/_base-button.vue
@@ -1,4 +1,46 @@
 <script>
+const classColorScheme = ({ flat = false, colorType = 'default' }) =>  {
+  let colorScheme = {}
+
+  if(flat) {
+    colorScheme = Object.assign({}, colorScheme, {
+      'text-grey hover:text-grey-light': colorType === 'default',
+      'text-blue hover:text-blue-light': colorType === 'primary',
+      'text-green hover:text-green-light': colorType === 'accent',
+      'text-green hover:text-green-light': colorType === 'success',
+      'text-red hover:text-red-light': colorType === 'danger',
+      'text-yellow hover:text-yellow-light': colorType === 'warning',
+      'text-blue-light hover:text-blue-lighter': colorType === 'info'
+    })
+  } else {
+    colorScheme = Object.assign({}, colorScheme, {
+      'rounded shadow': true,
+      'bg-grey-light hover:bg-grey': colorType === 'default',
+      'bg-blue text-white hover:bg-blue-light': colorType === 'primary',
+      'bg-green text-white hover:bg-green-light': colorType === 'accent',
+      'bg-green text-white hover:bg-grey-light': colorType === 'success',
+      'bg-red text-white hover:bg-red-light': colorType === 'danger',
+      'bg-yellow text-white hover:bg-yellow-light': colorType === 'warning',
+      'bg-blue-light text-white hover:bg-blue-lighter': colorType === 'info'
+    })
+  }
+
+  return colorScheme
+}
+
+const classSize = ({ size = 'md', flat = false}) => {
+  if(flat) {
+    return ''
+  }
+
+  if(size === 'sm') {
+    return 'px-4 py-2'
+  } else if(size === 'md') {
+    return 'px-5 py-3'
+  } else if(size === 'lg') {
+    return 'px-6 py-4'
+  }
+}
 export default {
   functional: true,
   name: 'BaseButton',
@@ -6,6 +48,22 @@ export default {
     color: {
       type: String,
       default: 'default'
+    },
+    flat: {
+      type: Boolean,
+      default: false
+    },
+    sm: {
+      type: Boolean,
+      default: false
+    },
+    md: {
+      type: Boolean,
+      default: false
+    },
+    lg: {
+      type: Boolean,
+      default: false
     },
     waiting: {
       type: Boolean,
@@ -23,16 +81,15 @@ export default {
     return createElement(
       'button',
       {
-        class: {
-          'py-2 px-4 rounded focus:outline-none focus:shadow-outline': true,
-          'bg-grey-light hover:bg-grey': context.props.color === 'default',
-          'bg-blue text-white': context.props.color === 'primary',
-          'bg-green text-white': context.props.color === 'accent',
-          'bg-green text-white': context.props.color === 'success',
-          'bg-red text-white': context.props.color === 'danger',
-          'bg-yellow text-white': context.props.color === 'warning',
-          'bg-blue-light text-white': context.props.color === 'info'
-        },
+        class: [
+          'focus:outline-none',
+          classColorScheme({flat: context.props.flat, colorType: context.props.color}),
+          classSize({
+            size: context.props.lg ? 'lg' : (context.props.md ? 'md' : (context.props.sm ? 'sm' : undefined)),
+            flat: context.props.flat
+          }),
+          context.data.staticClass
+        ],
         attrs: context.props,
         on: context.listeners
       },
