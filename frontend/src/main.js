dcb5785c720494db60fa3751bfde695c29254b08
Bonnak <nak2bo@yahoo.com>
2018-09-09 16:07:35 +0700

    Manage user - install vmodal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,16 @@
 import './styles/main.css'
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import vmodal from 'vue-js-modal'
 import App from './components/App.vue'
 import router from './router'
 import store from './store'
 import './components/common'
 
-Vue.use(Vuelidate)
-
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
+Vue.use(vmodal, { dynamic: true, injectModalsContainer: true })
 
 new Vue({
   router,
