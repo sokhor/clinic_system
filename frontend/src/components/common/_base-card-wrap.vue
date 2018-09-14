c6eda267250886cc5282b32d6f50e8f52a973616
Bonnak <nak2bo@yahoo.com>
2018-09-15 00:01:07 +0700

    Card wrap component

---
+++ frontend/src/components/common/_base-card-wrap.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'BaseCardWrap',
+  functional: true,
+  render(h, context) {
+    return (
+      <div class="p-4" {...{ class: context.data.staticClass }}>
+        {context.children}
+      </div>
+    )
+  }
+}
+</script>
