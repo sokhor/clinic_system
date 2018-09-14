5710e88d7a1d096839bf3c3c448ba990d940af3e
Bonnak <nak2bo@yahoo.com>
2018-09-14 14:37:27 +0700

    Card component

---
+++ frontend/src/components/common/_base-card.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'BaseCard',
+  functional: true,
+  render(h, context) {
+    return (
+      <div
+        class="w-full bg-white shadow rounded overflow-hidden"
+        {...{ class: context.data.staticClass }}
+      >
+        { context.children }
+      </div>
+    )
+  }
+}
+</script>
