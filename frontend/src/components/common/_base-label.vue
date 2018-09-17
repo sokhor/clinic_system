31fbaadc0ed471d1ae5c7aaf78af3840a13176ff
Bonnak <nak2bo@yahoo.com>
2018-09-17 14:23:53 +0700

    Base label

---
+++ frontend/src/components/common/_base-label.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'BaseLabel',
+  functional: true,
+  render(h, context) {
+    return (
+      <label
+        class="block text-grey-darker text-sm font-bold"
+        {...{ class: context.data.staticClass }}
+      >
+        {context.children}
+      </label>
+    )
+  }
+}
+</script>
