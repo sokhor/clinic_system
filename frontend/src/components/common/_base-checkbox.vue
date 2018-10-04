8ab43d6651cec19deda8937beae51f5db4240668
Bonnak <nak2bo@yahoo.com>
2018-10-04 11:08:35 +0700

    Fix ischeck checkbox and can disable

--- frontend/src/components/common/_base-checkbox.vue
+++ frontend/src/components/common/_base-checkbox.vue
@@ -1,8 +1,12 @@
 <script>
+import { findIndex, isEqual } from 'lodash'
+
 const isChecked = context => {
   return (
     (Array.isArray(context.data.model.value) &&
-      context.data.model.value.indexOf(context.data.attrs.value) >= 0) ||
+      findIndex(context.data.model.value, o =>
+        isEqual(o, context.data.attrs.value)
+      ) >= 0) ||
     context.data.model.value === true ||
     (context.data.attrs && context.data.attrs.checked)
   )
@@ -28,22 +32,34 @@ const onInput = (event, context) => {
 export default {
   name: 'BaseCheckbox',
   functional: true,
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
   render(h, context) {
     return (
       <div
         class="relative block h-6 w-6 pl-6 flex items-center"
         {...{
-          class: [isChecked(context) ? 'checked' : '', context.data.staticClass]
+          class: [
+            context.data.staticClass,
+            { checked: isChecked(context) },
+            { disabled: context.props.disabled }
+          ]
         }}
       >
         <input
           type="checkbox"
           class="absolute z--1 opacity-0"
           checked={isChecked(context)}
+          disabled={context.props.disabled}
+          onChange={context.listeners['change'] || (() => {})}
           onInput={event => onInput(event, context)}
         />
         <div
-          class="box-checkbox absolute block w-4 h-4 rounded cursor-pointer select-none pin-l border bg-white-grey-lighter"
+          class="box-checkbox absolute block w-4 h-4 rounded select-none pin-l border bg-white-grey-lighter"
           onClick={event => event.target.previousSibling.click()}
         />
         <label class="relative mb-0 whitespace-no-wrap">
@@ -70,6 +86,18 @@ input[type='checkbox']:checked ~ .box-checkbox {
 
 input[type='checkbox']:checked ~ .box-checkbox:after {
   background-image: url(data:image/svg+xml;charset=utf8;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHZpZXdCb3g9JzAgMCA4IDgnPjxwYXRoIGZpbGw9JyNmZmYnIGQ9J002LjU2NC43NWwtMy41OSAzLjYxMi0xLjUzOC0xLjU1TDAgNC4yNiAyLjk3NCA3LjI1IDggMi4xOTN6Jy8+PC9zdmc+);
+}
+
+input[type='checkbox']:disabled ~ .box-checkbox {
+  @apply bg-grey;
+}
+
+input[type='checkbox']:disabled ~ .box-checkbox:hover {
+  @apply cursor-not-allowed;
+}
+
+.box-checkbox:hover {
+  @apply cursor-pointer;
 }
 
 .box-checkbox:after {
