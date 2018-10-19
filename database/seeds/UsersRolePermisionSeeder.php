45d6d775f88f7c715786ee317e6d9b7748c411e6
Bonnak <nak2bo@yahoo.com>
2018-10-19 21:16:16 +0700

    Seed ward permission data

--- database/seeds/UsersRolePermisionSeeder.php
+++ database/seeds/UsersRolePermisionSeeder.php
@@ -19,18 +19,9 @@ class UsersRolePermisionSeeder extends Seeder
             'username' => 'superadmin',
             'email' => 'superadmin@example.com',
         ]);
-
         Bouncer::allow($user)->everything();
-        Bouncer::ability()->firstOrCreate(['name' => 'views-users', 'title' => 'View users']);
-        Bouncer::ability()->firstOrCreate(['name' => 'create-users', 'title' => 'Create users']);
-        Bouncer::ability()->firstOrCreate(['name' => 'edit-users', 'title' => 'Edit users']);
-        Bouncer::ability()->firstOrCreate(['name' => 'delete-users', 'title' => 'Delete users']);
-        Bouncer::ability()->firstOrCreate(['name' => 'reset-password-users', 'title' => 'Reset password users']);
-        Bouncer::ability()->firstOrCreate(['name' => 'attach-roles-users', 'title' => 'Attach users roles']);
-        Bouncer::ability()->firstOrCreate(['name' => 'view-roles', 'title' => 'View roles']);
-        Bouncer::ability()->firstOrCreate(['name' => 'create-roles', 'title' => 'Create roles']);
-        Bouncer::ability()->firstOrCreate(['name' => 'edit-roles', 'title' => 'Edit roles']);
-        Bouncer::ability()->firstOrCreate(['name' => 'delete-roles', 'title' => 'Delete roles']);
+
+        $this->seedPermissions();
 
         Artisan::call('passport:client', ['--password' => true, '--no-interaction' => true]);
 
@@ -38,5 +29,30 @@ class UsersRolePermisionSeeder extends Seeder
         $client->update(['user_id' => $user->id]);
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    }
+
+    /**
+     * Seed roles data
+     *
+     * @return void
+     */
+    protected function seedPermissions()
+    {
+        Bouncer::ability()->firstOrCreate(['name' => 'views-users', 'title' => 'View users']);
+        Bouncer::ability()->firstOrCreate(['name' => 'create-users', 'title' => 'Create users']);
+        Bouncer::ability()->firstOrCreate(['name' => 'edit-users', 'title' => 'Edit users']);
+        Bouncer::ability()->firstOrCreate(['name' => 'delete-users', 'title' => 'Delete users']);
+        Bouncer::ability()->firstOrCreate(['name' => 'reset-password-users', 'title' => 'Reset password users']);
+        Bouncer::ability()->firstOrCreate(['name' => 'attach-roles-users', 'title' => 'Attach users roles']);
+
+        Bouncer::ability()->firstOrCreate(['name' => 'view-roles', 'title' => 'View roles']);
+        Bouncer::ability()->firstOrCreate(['name' => 'create-roles', 'title' => 'Create roles']);
+        Bouncer::ability()->firstOrCreate(['name' => 'edit-roles', 'title' => 'Edit roles']);
+        Bouncer::ability()->firstOrCreate(['name' => 'delete-roles', 'title' => 'Delete roles']);
+
+        Bouncer::ability()->firstOrCreate(['name' => 'view-wards', 'title' => 'View wards']);
+        Bouncer::ability()->firstOrCreate(['name' => 'create-wards', 'title' => 'Create wards']);
+        Bouncer::ability()->firstOrCreate(['name' => 'update-wards', 'title' => 'Update wards']);
+        Bouncer::ability()->firstOrCreate(['name' => 'delete-wards', 'title' => 'Delete wards']);
     }
 }
