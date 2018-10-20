cd220c20176336f12c1918c5daa2ebbc447f9c81
Bonnak <nak2bo@yahoo.com>
2018-10-20 23:29:09 +0700

    Mixin of stote

---
+++ frontend/src/store/modules/_mixin.js
@@ -0,0 +1,40 @@
+function setPaging({
+  current_page = 1,
+  from = null,
+  to = null,
+  last_page = 0,
+  per_page = 15,
+  total = 0
+} = {}) {
+  return {
+    current_page,
+    from,
+    to,
+    last_page,
+    per_page,
+    total
+  }
+}
+
+export const baseState = {
+  resources: [],
+  search: '',
+  pagination: {}
+}
+
+export const baseMutations = {
+  RECEIVE_RESOURCES(state, { data, meta }) {
+    state.resources = []
+    data.forEach(resource => {
+      state.resources.push(Object.assign(resource, { _deleting: false }))
+    })
+
+    state.pagination = setPaging(meta)
+  },
+  SET_SEARCH(state, search) {
+    state.search = search
+  },
+  RESET_CURRENT_PAGE(state) {
+    state.pagination.current_page = 1
+  }
+}
