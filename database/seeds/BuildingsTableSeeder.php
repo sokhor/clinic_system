e88d2204cdeab57a6d6ab65a3f42974a7864d1dc
Bonnak <nak2bo@yahoo.com>
2018-10-21 20:53:59 +0700

    Seed building table

---
+++ database/seeds/BuildingsTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use App\Place\Models\Building;
+
+class BuildingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('building_ward')->truncate();
+        Building::truncate();
+
+        factory(Building::class)->create([
+            'name_kh' => 'អគារ៤អា',
+            'name_en' => 'House 4A',
+        ]);
+
+        factory(Building::class)->create([
+            'name_kh' => 'អគារ២អា',
+            'name_en' => 'House 2A',
+        ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionsSeeder::class);
         $this->call(UsersRolePermisionSeeder::class);
         $this->call(WardsTableSeeder::class);
+        $this->call(BuildingsTableSeeder::class);
     }
 }
