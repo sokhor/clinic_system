f50828a6d4fabf8fc001a0cd910cb5a5b3c84d68
Bonnak <nak2bo@yahoo.com>
2018-10-26 01:07:10 +0700

    Change timezone to asia/Phnom_Penh

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'asia/Phnom_Penh',
 
     /*
     |--------------------------------------------------------------------------
@@ -155,6 +155,7 @@ return [
          * Application Service Providers...
          */
         App\Place\Providers\RouteServiceProvider::class,
+        App\Reception\Providers\ReceptionServiceProvider::class,
 
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
