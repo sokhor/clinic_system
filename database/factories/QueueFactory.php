e978b23b46bca8289c406ae3c74ddeb3c6b3a757
Bonnak <nak2bo@yahoo.com>
2018-10-27 21:00:20 +0700

    Fix queue factory to have patient id

--- database/factories/QueueFactory.php
+++ database/factories/QueueFactory.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Reception\Models\Appointment;
+use App\Reception\Models\Patient;
 use Faker\Generator as Faker;
 
 $factory->define(App\Reception\Models\Queue::class, function (Faker $faker) {
     return [
-        'appointment_id' => function() { return factory(Appointment::class)->create()->id; },
+        'patient_id' => function() { return factory(Patient::class)->create()->id; },
         'queue_no' => random_int(100, 300),
         'alive' => true,
     ];
