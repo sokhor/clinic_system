ab083e5ced0860647478e9e4a3599ae2d28aa1e0
Bonnak <nak2bo@yahoo.com>
2018-10-27 21:03:47 +0700

    Check permission for registering permission and validate required fields

--- tests/Feature/Reception/RegistrationTest.php
+++ tests/Feature/Reception/RegistrationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Reception;
 use App\Reception\Models\Appointment;
 use App\Reception\Models\Patient;
 use App\Reception\Models\Queue;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Carbon;
@@ -53,13 +54,65 @@ class RegistrationTest extends TestCase
             'type' => $appointment->type,
             'status' => $appointment->status,
         ]);
+    }
 
-        $appointment = Appointment::first();
+    /** @test */
+    function it_register_a_patient_and_dont_meet_required_fields()
+    {
+        $user = factory(User::class)->create();
+        $user->allow('create-patients');
+        $this->signIn($user);
 
-        $this->assertDatabaseHas('queues', [
-            'appointment_id' => $appointment->id,
-            'queue_no' => $queue->queue_no,
-            'alive' => $queue->alive,
+        $this->postJson('api/reception/register', [])
+        ->assertStatus(422)
+        ->assertJsonValidationErrors([
+            'name_kh',
+            'name_en',
+            'gender',
+            'nationality_code',
+            'phone',
+            'identity_type',
+            'identity_no',
+        ]);
+
+        $this->assertCount(0, Patient::all());
+        $this->assertCount(0, Queue::all());
+    }
+
+    /** @test */
+    function it_not_allow_to_register_a_new_patient()
+    {
+        $this->signIn();
+
+        $patient = factory(Patient::class)->make();
+        $queue = factory(Queue::class)->make([
+            'patient_id' => null,
+            'queue_no' => null
+        ]);
+
+        $this->postJson('api/reception/register', array_merge(
+            $patient->toArray(),
+            $queue->toArray()
+        ))->assertStatus(403);
+
+        $this->assertDatabaseMissing('patients', [
+            'name_kh' => $patient->name_kh,
+            'name_en' => $patient->name_en,
+            'dob' => $patient->dob,
+            'gender' => $patient->gender,
+            'nationality_code' => $patient->nationality_code,
+            'phone' => $patient->phone,
+            'email' => $patient->email,
+            'address' => $patient->address,
+            'identity_type' => $patient->identity_type,
+            'identity_no' => $patient->identity_no,
+            'last_visited_at' => $patient->last_visited_at,
+            'referal' => $patient->referal,
+        ]);
+
+        $this->assertDatabaseMissing('queues', [
+            'queue_no' => 1,
+            'alive' => true,
         ]);
     }
 }
