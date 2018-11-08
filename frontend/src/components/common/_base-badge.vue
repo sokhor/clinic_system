72a3395c01905d3529b3720353b06f96ea4f6c1e
Bonnak <nak2bo@yahoo.com>
2018-11-09 03:05:47 +0700

    Badge component

---
+++ frontend/src/components/common/_base-badge.vue
@@ -0,0 +1,33 @@
+<script>
+const colorClass = color => {
+  if (color.toLowerCase() === 'blue') {
+    return 'bg-blue text-white'
+  } else if (color.toLowerCase() === 'red') {
+    return 'bg-red text-white'
+  } else if (color.toLowerCase() === 'green') {
+    return 'bg-green text-white'
+  }
+}
+
+export default {
+  name: 'BaseBadge',
+  functional: true,
+  props: {
+    color: {
+      type: String,
+      default: ''
+    }
+  },
+  render(h, context) {
+    return (
+      <div
+        class="p-1 rounded inline-block font-bold leading-none text-center align-baseline whitespace-no-wrap text-75%"
+        {...{ class: context.data.staticClass }}
+        {...{ class: colorClass(context.props.color) }}
+      >
+        {context.children}
+      </div>
+    )
+  }
+}
+</script>
