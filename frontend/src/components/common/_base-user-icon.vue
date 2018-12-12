cfdf839c443738955d2b54493e8923f94e350024
Bonnak <nak2bo@yahoo.com>
2018-12-12 22:39:50 +0700

    user icon

---
+++ frontend/src/components/common/_base-user-icon.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'BaseUserIcon',
+  functional: true,
+  render: (h, context) => (
+    <svg
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      class="fill-current text-grey-dark"
+      {...{ class: context.data.staticClass }}
+    >
+      <circle cx="16" cy="7.5" r="7.4" />
+      <path d="M16,18C7.7,18,1,24.3,1,32h30C31,24.3,24.3,18,16,18z" />
+    </svg>
+  )
+}
+</script>
