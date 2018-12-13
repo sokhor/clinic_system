6e7b198629d086e0ee8a624d23c1cebd851b6462
Bonnak <chea.bonnak@gmail.com>
2018-12-13 23:25:33 +0700

    Create medical records and medications table

---
+++ database/migrations/2018_12_13_224311_create_medical_records_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicalRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medical_records', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('patient_id');
+            $table->string('marital_status')->nullable();
+            $table->string('number_of_children')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('height')->nullable();
+            $table->string('weigh')->nullable();
+            $table->boolean('smoking')->nullable();
+            $table->string('smoking_info')->nullable();
+            $table->boolean('alcoholic')->nullable();
+            $table->string('alcoholic_info')->nullable();
+            $table->text('patient_complaint');
+            $table->float('blood_pressure_systolic');
+            $table->float('blood_pressure_diastolic');
+            $table->float('pulse');
+            $table->float('temperature');
+            $table->float('respiratory_rate');
+            $table->float('pso2');
+            $table->text('anc_note')->nullable();
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medical_records');
+    }
+}
---
+++ database/migrations/2018_12_13_231552_create_medications_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('medical_record_id');
+            $table->string('brand_name');
+            $table->string('composition_name');
+            $table->string('composition_strength');
+            $table->string('dosage');
+            $table->string('frequency');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medications');
+    }
+}
