731da751ae2602b9e9f2a06971c9bb7751980818
Bonnak <chea.bonnak@gmail.com>
2018-12-14 09:26:28 +0700

    Darken shadow

--- frontend/tailwind.js
+++ frontend/tailwind.js
@@ -727,10 +727,10 @@ module.exports = {
   */
 
   shadows: {
-    default: '0 2px 4px 0 rgba(0,0,0,0.10)',
-    md: '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
-    lg: '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
-    inner: 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
+    default: '0 1px 3px 0 rgba(0,0,0,.3)',
+    md: '0 2px 6px 0 rgba(0,0,0,0.32), 0 1px 3px 0 rgba(0,0,0,.28)',
+    lg: '0 10px 30px 0 rgba(0,0,0,0.32), 0 5px 10px 0 rgba(0,0,0,.28)',
+    inner: 'inset 0 1px 3px 0 rgba(0,0,0,0.26)',
     outline: '0 0 0 3px rgba(52,144,220,0.5)',
     none: 'none'
   },
