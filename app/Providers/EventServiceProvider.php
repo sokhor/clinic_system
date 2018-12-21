670f2af2e9be09469176f777b25d4817adee31b6
Bonnak <chea.bonnak@gmail.com>
2018-12-21 12:44:16 +0700

    Upgrade to laravel 5.7

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -13,6 +15,9 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
+        Registered::class => [
+            SendEmailVerificationNotification::class,
+        ],
         'App\Place\Events\BuildingDeleted' => [
             'App\Place\Listeners\BuildingWardsPivot',
         ],
