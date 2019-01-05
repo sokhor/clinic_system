c2352aef02e62483af70dfe2acc5c61ee565af74
Bonnak <chea.bonnak@gmail.com>
2019-01-06 00:27:17 +0700

    Scope doctor

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Staff extends Model
@@ -15,4 +16,19 @@ class Staff extends Model
      * @var string
      */
     protected $table = 'staffs';
+
+    /**
+     * Query doctors
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeDoctor(Builder $query)
+    {
+        return $query->whereExists(function($query) {
+            return $query->from('positions')
+            ->whereRaw("positions.id = {$this->table}.position_id")
+            ->whereRaw("lower(name) = lower('Doctor')");
+        });
+    }
 }
