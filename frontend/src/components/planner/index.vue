5daa5cd449bfe537b01b6a549e9d2cef8c20d619
Bonnak <chea.bonnak@gmail.com>
2019-01-09 00:33:00 +0700

    Planner week

--- frontend/src/components/planner/index.vue
+++ frontend/src/components/planner/index.vue
@@ -15,20 +15,27 @@
         :current-date="currentDate"
         :events="events"
         @date-title="val => title = val"
-        v-show="buttonState === 'month'"
+        v-if="buttonState === 'month'"
+      />
+      <planner-week
+        :current-date="currentDate"
+        :events="events"
+        @date-title="val => title = val"
+        v-if="buttonState === 'week'"
       />
     </section>
   </div>
 </template>
 
 <script>
-import PlannerMonth from './planner-month.vue'
 import DateNavigator from './date-navigator.vue'
 import PlannerMode from './planner-mode.vue'
+import PlannerMonth from './planner-month.vue'
+import PlannerWeek from './planner-week.vue'
 
 export default {
   name: 'Planner',
-  components: { PlannerMonth, DateNavigator, PlannerMode },
+  components: { DateNavigator, PlannerMode, PlannerMonth, PlannerWeek },
   props: {
     events: { default: () => [] }
   },
