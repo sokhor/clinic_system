f0a40ecef6737dc4780ef57017ad0a7b517530be
Bonnak <chea.bonnak@gmail.com>
2019-01-08 23:38:59 +0700

    Simple scrollbar

---
+++ frontend/src/components/simple-scrollbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="simple-scrollbar">
+    <div class="simplebar-wrapper">
+        <div class="simplebar-height-auto-observer-wrapper">
+          <div class="simplebar-height-auto-observer" />
+        </div>
+        <div class="simplebar-mask">
+          <div class="simplebar-offset">
+            <div class="simplebar-content">
+              <slot></slot>
+            </div>
+          </div>
+        </div>
+        <div class="simplebar-placeholder" />
+      </div>
+      <div class="simplebar-track simplebar-horizontal">
+        <div class="simplebar-scrollbar" />
+      </div>
+      <div class="simplebar-track simplebar-vertical">
+        <div class="simplebar-scrollbar" />
+      </div>
+  </div>
+</template>
+
+<script>
+import SimpleBar from 'simplebar'
+
+export default {
+  name: 'SimplebScrollbar',
+  data() {
+    return {
+      scrollbar: null
+    }
+  },
+  mounted() {
+    this.scrollbar = new SimpleBar(this.$refs['simple-scrollbar'])
+  }
+}
+</script>
