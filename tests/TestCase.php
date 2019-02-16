48c4ef69eb33b67b38c38f8830d9851c1c18be88
Bonnak <chea.bonnak@gmail.com>
2019-02-16 20:45:21 +0700

    Add method allowEverything and allow

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,29 +2,56 @@
 
 namespace Tests;
 
+use App\User;
+use Bouncer;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\Passport\Passport;
-use Bouncer;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    protected function signIn($user = null)
-    {
-        $user = $user ?: factory(\App\User::class)->create();
+    protected $user;
 
-        Passport::actingAs($user);
+    protected function signInSuperAdmin($admin = null)
+    {
+        $admin = $admin ?: factory(User::class)->create(['username' => 'superadmin']);
+        Bouncer::allow($admin)->everything();
+
+        Passport::actingAs($admin);
 
         return $this;
     }
 
-    protected function signInSuperAdmin($admin = null)
-    {
-        $admin = $admin ?: factory(\App\User::class)->create(['username' => 'superadmin']);
-        Bouncer::allow($admin)->everything();
 
-        Passport::actingAs($admin);
+    protected function signIn($user = null)
+    {
+        $this->user = $user ?: $this->user ?: factory(User::class)->create();
+
+        Passport::actingAs($this->user);
+
+        return $this;
+    }
+
+    protected function allowEverything($user = null)
+    {
+        $this->user = $user ?: $this->user ?: factory(User::class)->create();
+
+        Bouncer::allow($this->user)->everything();
+
+        return $this;
+    }
+
+    protected function allow($ability, $model = null, $user = null)
+    {
+        $this->user = $user ?: $this->user ?: factory(User::class)->create();
+
+        if(is_null($model)) {
+            Bouncer::allow($this->user)->to($ability);
+        }
+        else {
+            Bouncer::allow($this->user)->to($ability, $model);
+        }
 
         return $this;
     }
