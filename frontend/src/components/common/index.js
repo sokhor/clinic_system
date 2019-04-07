e7d101cdcea0dc668b58dc4e0dbe9140919476cd
Bonnak <chea.bonnak@gmail.com>
2019-04-07 11:51:21 +0700

    accept .vue, .js and index

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import upperFirst from 'lodash/upperFirst'
 import camelCase from 'lodash/camelCase'
 
-const requireComponent = require.context('.', false, /_base-[\w-]+\.vue$/)
+const requireComponent = require.context('.', true, /_base-[\w-]+(.vue|.js|)$/)
 
 requireComponent.keys().forEach(fileName => {
   const componentConfig = requireComponent(fileName)
