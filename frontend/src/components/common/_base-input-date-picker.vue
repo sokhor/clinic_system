de75203efcb0316d7c23e68d7b94f24624a7af87
Bonnak <chea.bonnak@gmail.com>
2019-04-29 13:26:48 +0700

    Update date picker

--- frontend/src/components/common/_base-input-date-picker.vue
+++ frontend/src/components/common/_base-input-date-picker.vue
@@ -1,13 +1,18 @@
 <template>
   <on-click-outside :do="close">
     <div class="relative">
-      <base-input
+      <!-- <base-input
         type="text"
         placeholder="DD-MM-YYYY"
         v-bind="$attrs"
         v-model="value"
         @focus="show"
-      />
+      /> -->
+      <div class="flex items-center border rounded px-3 w-full date-input">
+        <input class="appearance-none py-2 text-gray-800 leading-none focus:outline-none w-full" @focus="show" v-model="day" placeholder="DD"/> - 
+        <input class="appearance-none py-2 text-gray-800 leading-none focus:outline-none w-full" @focus="show" v-model="month" placeholder="MM"/> - 
+        <input class="appearance-none py-2 text-gray-800 leading-none focus:outline-none w-full" @focus="show" v-model="year" placeholder="YYYY"/>
+      </div>
       <date-picker v-if="picker" class="absolute z-20" @change="selectDate" />
     </div>
   </on-click-outside>
@@ -24,7 +29,10 @@ export default {
   data() {
     return {
       picker: false,
-      value: null
+      value: null,
+      day: '',
+      month: '',
+      year: ''
     }
   },
   methods: {
@@ -36,6 +44,12 @@ export default {
     },
     selectDate(date) {
       this.value = date.format('DD-MM-YYYY')
+      
+      let dateArray = this.value.split('-')
+      this.day = dateArray[0]
+      this.month = dateArray[1]
+      this.year = dateArray[2]
+
       this.close()
     }
   },
@@ -46,3 +60,8 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+.date-input
+  @apply .shadow-outline
+</style>
