3bacb5992dd0b3b950559c00f3a15efe46194809
Bonnak <chea.bonnak@gmail.com>
2019-05-19 12:00:46 +0700

    Image response

---
+++ app/Http/Responses/ImageResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Responses;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Contracts\Support\Responsable;
+use Illuminate\Support\Facades\Storage;
+
+class ImageResponse implements Responsable
+{
+    protected $filePath;
+
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    public function toResponse($request)
+    {
+        $fileName = last(explode('/', $this->filePath));
+        $extension = last(explode('.', $fileName));
+
+        return response(Storage::get($this->filePath))
+                ->header('Content-Type', "image/$extension")
+                ->header('Content-Disposition', "inline; filename=\"$fileName\"");
+    }
+}
