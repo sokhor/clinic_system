97b441636d592c7af2a755e3f615f74ed8ff1306
Bonnak <chea.bonnak@gmail.com>
2019-05-25 22:17:13 +0700

    Move base select style to it own file and fix not focus

--- frontend/src/components/common/_base-select.vue
+++ frontend/src/components/common/_base-select.vue
@@ -1,104 +1,3 @@
-<style lang="scss">
-.search-select {
-  @apply relative;
-  // position: relative;
-}
-.search-select-input {
-  appearance: none;
-  text-align: left;
-  cursor: pointer;
-  user-select: none;
-  display: block;
-  width: 100%;
-  height: 34px;
-  padding: 6px 12px;
-  font-size: 12px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 0;
-  box-shadow: none;
-  border-color: #d2d6de;
-  white-space: nowrap;
-  display: flex;
-  align-items: center;
-
-  &:focus {
-    border-color: #3c8dbc;
-  }
-}
-.search-select-placeholder,
-.search-select-text {
-  flex-grow: 1;
-}
-.search-select__arrow {
-  transition: all 200ms ease-in-out;
-
-  &::before {
-    display: block;
-    color: #999;
-    border-style: solid;
-    border-width: 5px 5px 0;
-    border-color: #999 transparent transparent;
-    content: '';
-  }
-
-  &__open {
-    transform: rotate(180deg);
-  }
-}
-.search-select-dropdown {
-  margin-top: 0.25rem;
-  margin-bottom: 0.25rem;
-  position: absolute;
-  right: 0;
-  left: 0;
-  background-color: #fff;
-  padding: 0.5rem;
-  border-radius: 0.25rem;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
-  z-index: 50;
-  border: solid 1px #d2d6de;
-}
-.search-select-search {
-  display: block;
-  margin-bottom: 0.5rem;
-  width: 100%;
-  padding: 0.5rem 0.75rem;
-  background-color: #ededed;
-  border: 1px solid #ccc;
-
-  // &:focus {
-  //   border-color: #3c8dbc;
-  // }
-}
-.search-select-options {
-  list-style: none;
-  padding: 0;
-  position: relative;
-  overflow-y: auto;
-  max-height: 14rem;
-}
-.search-select-option {
-  padding: 0.5rem 0.75rem;
-  cursor: pointer;
-  border-radius: 0.25rem;
-  user-select: none;
-
-  &:hover {
-    background-color: #279add;
-    color: #fff;
-  }
-
-  &.is-active,
-  &.is-active:hover {
-    // background-color: #3490dc;
-  }
-}
-</style>
-
 <template>
   <on-click-outside :do="close">
     <div class="search-select" :class="{ 'is-active': isOpen }">
@@ -107,6 +6,8 @@
         @click="open"
         type="button"
         class="search-select-input"
+        :class="[{ 'search-select-input__disabled': disabled }]"
+        :disabled="disabled"
       >
         <span v-if="option !== null" class="search-select-text">{{
           typeof option === 'object' ? option.text : option
@@ -137,16 +38,17 @@
             class="search-select-option"
             v-for="(option, i) in filteredOptions"
             :key="typeof option === 'object' ? option.value : option"
-            @click="select(option)"
+            @click="select(option, i)"
             :class="{ 'is-active': i === highlightedIndex }"
           >
             {{ typeof option === 'object' ? option.text : option }}
           </li>
         </ul>
         <div v-show="filteredOptions.length === 0" class="search-select-empty">
-          <slot name="no-result" :search="search"
-            >No results found for "{{ search }}"</slot
-          >
+          <slot name="no-result" :search="search">
+            No results found for
+            <b>" {{ search }}"</b>
+          </slot>
         </div>
       </div>
     </div>
@@ -162,12 +64,12 @@ export default {
   components: {
     OnClickOutside
   },
-  props: ['value', 'options', 'filterFunction'],
+  props: ['value', 'options', 'filterFunction', 'disabled'],
   data() {
     return {
       isOpen: false,
       search: '',
-      highlightedIndex: 0,
+      highlightedIndex: null,
       option:
         this.options.find(
           option =>
@@ -187,21 +89,22 @@ export default {
       return this.options.filter(option => {
         option = typeof option === 'object' ? option.text : option
 
-        return `${option}`.toLowerCase().startsWith(this.search.toLowerCase())
+        return option.toLowerCase().startsWith(this.search.toLowerCase())
       })
     }
   },
   methods: {
     open() {
-      if (this.isOpen) {
-        return
+      if (!this.isOpen) {
+        this.isOpen = true
+        this.$nextTick(() => {
+          this.setupPopper()
+          this.$refs.search.focus()
+          this.scrollToHighlighted()
+        })
+      } else {
+        this.isOpen = false
       }
-      this.isOpen = true
-      this.$nextTick(() => {
-        this.setupPopper()
-        this.$refs.search.focus()
-        this.scrollToHighlighted()
-      })
     },
     setupPopper() {
       if (this.popper === undefined) {
@@ -234,11 +137,11 @@ export default {
       this.isOpen = false
       this.$refs.button.focus()
     },
-    select(option) {
+    select(option, index) {
       this.$emit('input', typeof option === 'object' ? option.value : option)
       this.$emit('change', typeof option === 'object' ? option.value : option)
       this.search = ''
-      this.highlightedIndex = 0
+      this.highlightedIndex = index
       this.option = option
       this.close()
     },
@@ -246,6 +149,8 @@ export default {
       this.select(this.filteredOptions[this.highlightedIndex])
     },
     scrollToHighlighted() {
+      if (this.highlightedIndex === null) return
+
       this.$refs.options.children.length > 0 &&
         this.$refs.options.children[this.highlightedIndex].scrollIntoView({
           block: 'nearest'
